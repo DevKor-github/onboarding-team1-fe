4d9aa6035d6dfd5a229c9ecf7f373e70d5b9c6d4
wjddnjs7895 <53262722+wjddnjs7895@users.noreply.github.com>
2024-08-13 23:53:59 +0900

    feat: add main header logo

--- src/features/chat/components/ChatHeader.tsx
+++ src/features/chat/components/ChatHeader.tsx
@@ -1,12 +1,15 @@
 import { RoundProfile } from './RoundProfile';
+import MainLogo from '@assets/logo_main.svg';
 
 export const ChatHeader = ({ userName, imgUrl }: { userName: string; imgUrl: string }) => {
   return (
-    <header className="h-32 shadow">
-      <div className="flex h-16 items-center px-6">DEVKOR LOGO</div>
-      <div className="flex h-16 items-center justify-between px-6">
+    <header className="flex flex-col shadow">
+      <div className="my-3 ml-5 w-20">
+        <MainLogo />
+      </div>
+      <div className="my-4 flex h-8 items-center justify-between px-6">
         <div className="flex items-center">
-          <RoundProfile imgUrl={imgUrl} />
+          <RoundProfile imgUrl={imgUrl} size="small" />
           <div className="mx-2">{userName}</div>
         </div>
         <>dots</>
