3e561f65cffc32a284c5680b9955a8ba0b1e425c
wjddnjs7895 <wjddnjs7895@naver.com>
2024-08-17 11:28:25 +0000

    feat: chat send/receive api

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,4 +1,4 @@
-import { DefaultOptions, QueryClient } from '@tanstack/react-query';
+import { DefaultOptions, QueryClient, UseMutationOptions } from '@tanstack/react-query';
 
 export const queryConfig = {
   queries: {
@@ -9,4 +9,10 @@ export const queryConfig = {
   },
 } satisfies DefaultOptions;
 
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> = Awaited<ReturnType<FnType>>;
+
 export const queryClient = new QueryClient({ defaultOptions: queryConfig });
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<ReturnType<T>, 'queryKey' | 'queryFn'>;
+
+export type MutationConfig<MutationFnType extends (...args: any) => Promise<any>> = UseMutationOptions<ApiFnReturnType<MutationFnType>, Error, Parameters<MutationFnType>[0]>;
